d64508509c74994c3a0d37c9d77a1d0487c5df1e
Mathias Van de Pol <mathias.vandepol@cresta.ai>
2023-03-13 17:52:19 +0100

    add english

--- codenames/src/app/core/services/store/dictionary.store.ts
+++ codenames/src/app/core/services/store/dictionary.store.ts
@@ -3,24 +3,39 @@ import { tap } from 'rxjs/operators';
 import { StoreService } from './stores';
 import { CacheExpiry } from '../../util/cache-expiry';
 import { DictionaryService } from '../asset/dictionary.service';
+import { BehaviorSubject } from 'rxjs';
+
+export enum Language {
+    NL = "nl",
+    EN = "en"
+}
 
 @Injectable({ providedIn: 'root' })
 export class DictionaryStore extends StoreService<string[]> {
     private cacheExpiry = new CacheExpiry();
+    private _language = new BehaviorSubject<Language>(Language.NL);
+
 
     constructor(private restService: DictionaryService) {
         super();
     }
 
+    public getLanguage(){
+        return this._language
+    }
+
+    public changeLanguage(language: Language){
+        this._language.next(language)
+        this.fetchData(true)
+    }
+
     // get the cached data
-    public fetchData(): void {
-        if (!this.cacheExpiry.isExpired) {
+    public fetchData(forceExpiry: boolean = false): void {
+        if (!this.cacheExpiry.isExpired && !forceExpiry) {
             return;
         }
         this.setStateFetching();
-        this.restService
-            .getAll()
-            .pipe(
+        this.restService.getDictionary(this._language.value.toString()).pipe(
                 super.catchErrorAndReset(),
                 tap((next: string[]) => {
                     this.store$.next({
